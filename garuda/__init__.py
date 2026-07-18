5cb2cf3f2d9599d861c83a81959d4dbb4cb7f233
Arup Basak <109652575+ArupSEK@users.noreply.github.com>
2026-07-18 15:47:00 +0530

    Expose Garuda orchestrator

--- garuda/__init__.py
+++ garuda/__init__.py
@@ -1,2 +1,6 @@
-"""Garuda reconnaissance toolkit."""
+"""Garuda modular external security assessment platform."""
 
+from .orchestrator import Orchestrator
+
+__version__ = "1.0.0"
+__all__ = ["Orchestrator"]
